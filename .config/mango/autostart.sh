d07eb7d97d34ebf0558c1c29277f366ea530d3cf
Matthew Ahlschlager <matt.ahlschlager@gmail.com>
2026-02-21 08:21:02 -0600

    var + mango

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -3,7 +3,7 @@
 set +e
 
 # obs (uncomment below after reviewing mangowc wiki)
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=kde
 
 # notify
 swaync &
@@ -14,8 +14,11 @@ swaybg -i ~/walls/wall1.png >/dev/null 2>&1 &
 # topbar
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 
-# keep clipboard content 
+# keep clipboard content
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
 
 # Permission authentication (review on mangowc wiki)
 /usr/lib/xfce-polkit/xfce-polkit &
+
+#dropbox
+dropbox &
