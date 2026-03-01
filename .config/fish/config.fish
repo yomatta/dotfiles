574936dcfb7d94d68703f1194ddeaf5c932653aa
yomatta <matt.ahlschlager@gmail.com>
2026-03-01 13:48:03 -0600

    re-org dotfiles in prep of switching main system

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    # do nothing
+end
+
+set -gx EDITOR nvim
