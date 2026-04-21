25b26591622bbed6c14bbf4d1c158fc6b291c334
Matthew Ahlschlager <matt.ahlschlager@gmail.com>
2026-04-21 18:40:14 -0500

    mangowc noctalia incorporation

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -8,14 +8,17 @@ yomatta-set-windowmanager mango >/dev/null 2>&1
 # obs (uncomment below after reviewing mangowc wiki)
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=kde
 
+# noctalia shell startup
+qs -c noctalia-shell &
+
 # notify
-mako &
+# mako &
 
 # wallpaper
-swaybg -i ~/walls/background.png >/dev/null 2>&1 &
+#swaybg -i ~/walls/background.png >/dev/null 2>&1 &
 
 # topbar
-waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+#waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 
 # keep clipboard content
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
