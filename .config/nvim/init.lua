730b1081fb1f561e08565831b3dc161c25c6d62b
Matthew Ahlschlager <matt.ahlschlager@gmail.com>
2026-05-05 15:06:22 -0500

    added automatic linebreak to nvim init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- enable line break (word wrap, but not mid-word)
+vim.o.linebreak = true
+
+-- enable word wrap (break mid-word)
+-- vim.o.wrap = true
