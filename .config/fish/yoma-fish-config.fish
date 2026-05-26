cbc7fae0fdf467ce7e3547dfbe63c9b7cb10b37d
Matthew Ahlschlager <matt.ahlschlager@gmail.com>
2026-05-26 14:21:16 -0500

    yoma fish config, created it to be used as a central source for personal aliases. source it into base CachyOS fish config

--- .config/fish/yoma-fish-config.fish
+++ .config/fish/yoma-fish-config.fish
@@ -1,13 +1,10 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-function fish_greeting
-    # do nothing
-end
+# This config file is meant to be sourced into CachyOS base fish config. This is just used for personal aliases and whatnot.
 
 set -gx EDITOR nvim
 
 ## My aliases
 alias bsync="sudo rsync -aAXHv --delete --exclude={".cache/","Downloads/","tmp/","Dropbox/"} /home/ /media/backup/home/"
 alias bsyncetc="sudo rsync -aAXHv --delete /etc/ /media/backup/etc/"
+
+alias resta="sudo reboot now"
+alias gpull="git pull origin main"
